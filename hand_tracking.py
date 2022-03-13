9fffb781d2f1fd032dc129b4ad6ac80daf610388
Brendan-Towell <towellb14@gmail.com>
2022-03-13 16:33:40 -0700

    Create hand_tracking.py

---
+++ hand_tracking.py
@@ -0,0 +1,50 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=1,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            print(
+                f'Index finger tip coordinate: (',
+                f'{handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * 640}, '
+                f'{handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * 480})'
+            )
+            for id, lm in enumerate(handLms.landmark):
+                #if id == 8:     #corresponding to tip of the index finger, see https://google.github.io/mediapipe/solutions/hands.html for reference
+                    #print("index tip coords: ", handLms.landmark[mpHands.INDEX_FINGER_TIP].x, ", ", handLms.landmark[mpHands.INDEX_FINGER_TIP].y)
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #if id ==0:
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+    cv2.imshow("Image", img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
